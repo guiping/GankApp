0fac69be334f2f011ba4fe09942264998776382b
guiping <gping.vip@gmail.com>
2016-04-19 02:15:47 +0800

    搭建项目框架，首次提交

---
+++ app/src/main/java/so/lvy/app/gankapp/MainActivity.java
@@ -0,0 +1,110 @@
+package so.lvy.app.gankapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import so.lvy.app.gankapp.utils.SnackbarUtils;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.action_share);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SnackbarUtils.showSnackbar(view, "测试", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        SnackbarUtils.showSnackbar(v, "点击查看的效果--->>>");
+                    }
+                });
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_share) {  //分享
+
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.daily_data) {   //TODO 每日最新
+            // Handle the camera action
+        } else if (id == R.id.all_data) { //TODO 所有数据
+
+        } else if (id == R.id.android_data) { // TODO ANDROID
+
+        } else if (id == R.id.ios_data) {   // TODO IOS
+
+        } else if (id == R.id.front_data) { // TODO 前段
+
+        } else if (id == R.id.welfare_data) { // TODO 福利
+
+        } else if (id == R.id.video_rest_data) {  // TODO 休息视频
+
+        } else if (id == R.id.spread_resource_data) { // TODO 拓展资源
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
---
+++ app/src/main/java/so/lvy/app/gankapp/utils/SnackbarUtils.java
@@ -0,0 +1,18 @@
+package so.lvy.app.gankapp.utils;
+
+import android.support.design.widget.Snackbar;
+import android.view.View;
+
+/**
+ * Created by ping on 2016/4/19.
+ */
+public class SnackbarUtils {
+
+    public static void showSnackbar(View view, String message, View.OnClickListener listener){
+        Snackbar.make(view, message, Snackbar.LENGTH_SHORT)
+                .setAction("点击",listener).show();
+    }
+    public static void showSnackbar(View view, String message){
+        showSnackbar(view,message,null);
+    }
+}
